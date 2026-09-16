de50aaf2e36ac4d3af715a130a7202a1baa7b0bf
Caleb Kibiwott <calebkibiwott06@gmail.com>
2026-09-16 18:14:01 +0300

    express server update

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,48 @@
+// Authentication controllers /srs/controllers/authcontrollers.js
+import express from 'express';
+
+export const register = async (req, res) => {
+  try{
+
+      // Business logic
+
+  }catch(err){
+    console.log('Something went wrong.', err)
+  }
+}
+
+
+export const login = async (req, res) => {
+  try{
+    //  bussiness logic
+  }catch(err){
+    console.log('Something went wrong.', err);
+  }
+}
+
+export const logout = async (req, res) => {
+  try{
+
+  }catch(err){
+    console.log('Something went wrong.', err)
+  }
+}
+
+export const updatePassword = async (req, res) => {
+  try{
+
+  }catch(err){
+    console.log('Something went wrong.', err)
+  }
+}
+
+
+export const getMe = async (req, res) => {
+  try{
+
+   
+
+  }catch(err){
+    console.log('Something went wrong.', err)
+  }
+}
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { register }  from '../controllers/authControllers';
+import { login }  from '../controllers/authControllers';
+import { updatePassword, logout, getMe } from '../controllers/authControllers';
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.patch("/update-password", updatePassword);
+router.get("/me", getMe)
